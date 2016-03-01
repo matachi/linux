df4cd1c2912fc51c9feafbbb75698e4b6f826102
Daniel Jonsson <daniel@jonsson.xyz>
2016-03-01 11:49:40 +0100

    Add ifndef guard to rangefix.h

--- scripts/kconfig/rangefix.h
+++ scripts/kconfig/rangefix.h
@@ -1,5 +1,10 @@
+#ifndef RANGEFIX_H
+#define RANGEFIX_H
+
 #define LKC_DIRECT_LINK
 #include "lkc.h"
 
 int rangefix_init(const char *);
 void rangefix_run(struct symbol *sym, tristate val);
+
+#endif
