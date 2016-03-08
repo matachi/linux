8a950b9273128ca6b3a8111bfb39dcde0cef34b2
Daniel Jonsson <daniel@jonsson.xyz>
2016-03-08 13:35:31 +0100

    Fix the generate_diagnoses test

--- scripts/kconfig/rangefixtest.c
+++ scripts/kconfig/rangefixtest.c
@@ -5,6 +5,7 @@
 
 #define test_data(str) "scripts/kconfig/test_data/" str
 
+static void assert_diagnosis_present(GArray *diagnoses, unsigned int n, ...);
 
 START_TEST(test_load_config)
 {
@@ -23,27 +24,27 @@ END_TEST
 
 START_TEST(test_generate_diagnoses)
 {
-	GArray *diagnoses, *diagnos;
-	struct symbol *sym;
-	char *name;
-	unsigned int i;
+	/* There are 4 minimal unsatisfiable cores in this test:
+	 *
+	 * - [64BIT, DRIVER1]
+	 * - [DRIVER2, DRIVER4]
+	 * - [DRIVER2, DRIVER5]
+	 * - [DRIVER3, DRIVER5]
+	 *
+	 * A diagnosis covers at least one variable in each core. In this case
+	 * there are 6 minimal diagnoses that satisfy this. */
+	GArray *diagnoses, *diagnosis;
 
 	rangefix_init(test_data("Kconfig3"), test_data("dotconfig3"));
 	diagnoses = rangefix_generate_diagnoses();
-	ck_assert_int_eq(diagnoses->len, 1);
 
-	diagnos = g_array_index(diagnoses, GArray *, 0);
-	for (i = 0; i < diagnos->len; ++i) {
-		sym = g_array_index(diagnos, struct symbol *, i);
-		name = sym->name;
-		if (g_strcmp0(name, "DRIVER1") == 0 ||
-		    g_strcmp0(name, "DRIVER2") == 0 ||
-		    g_strcmp0(name, "DRIVER3") == 0) {
-			ck_assert(true);
-			continue;
-		}
-		ck_assert(false);
-	}
+	ck_assert_int_eq(diagnoses->len, 6);
+	assert_diagnosis_present(diagnoses, 3, "64BIT", "DRIVER2", "DRIVER3");
+	assert_diagnosis_present(diagnoses, 3, "64BIT", "DRIVER2", "DRIVER5");
+	assert_diagnosis_present(diagnoses, 3, "64BIT", "DRIVER4", "DRIVER5");
+	assert_diagnosis_present(diagnoses, 3, "DRIVER1", "DRIVER2", "DRIVER3");
+	assert_diagnosis_present(diagnoses, 3, "DRIVER1", "DRIVER2", "DRIVER5");
+	assert_diagnosis_present(diagnoses, 3, "DRIVER1", "DRIVER4", "DRIVER5");
 }
 END_TEST
 
@@ -80,4 +81,50 @@ int main(void) {
 	number_failed = srunner_ntests_failed(sr);
 	srunner_free(sr);
 	return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
+
+/* Check if a diagnosis is present in the diagnoses array.
+ *
+ * An example call:
+ *     assert_diagnosis_present(diagnoses, 3, "64BIT", "DRIVER4", "DRIVER5");
+ */
+static void assert_diagnosis_present(GArray *diagnoses, unsigned int n, ...) {
+	unsigned int i, j, k, names_in_diagnosis;
+	va_list ap;
+	GArray *input, *diagnosis;
+	struct symbol *sym;
+	char *name;
+
+	va_start(ap, n);
+	input = g_array_new(false, false, sizeof(char *));
+	for (i = 0; i < n; ++i) {
+		name = va_arg(ap, char *);
+		input = g_array_append_val(input, name);
+	}
+	va_end(ap);
+	assert(input->len == n);
+
+	/* Iterare over all diagnoses */
+	for (i = 0; i < diagnoses->len; ++i) {
+		diagnosis = g_array_index(diagnoses, GArray *, i);
+		names_in_diagnosis = 0;
+		/* Iterate over all symbols in the diagnosis */
+		for (j = 0; j < diagnosis->len; ++j) {
+			sym = g_array_index(diagnosis, struct symbol *, j);
+			/* Iterate over the names in the input */
+			for (k = 0; k < input->len; ++k) {
+				name = g_array_index(input, char *, k);
+				if (g_strcmp0(sym->name, name) == 0) {
+					++names_in_diagnosis;
+					break;
+				}
+			}
+		}
+		if (names_in_diagnosis == input->len) {
+			goto deallocate;
+		}
+	}
+	ck_assert(false);
+deallocate:
+	g_array_free(input, false);
 }
